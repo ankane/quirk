abcbc453adf05f3b2c0a9568862ff70302fbc8f1
Andrew Kane <acekane1@gmail.com>
2017-02-21 05:00:23 -0800

    Version bump to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='quirk',
-    version='0.1.0',
+    version='0.1.1',
     description='Your data science sidekick',
     url='https://github.com/ankane/quirk',
     author='Andrew Kane',
